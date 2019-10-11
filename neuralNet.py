b5400b671bbf1d4b2c96a6d33dab2f9fd779dfbe
Lennart Lehmann <lennart.lehmann@tum.de>
2019-10-11 11:11:21 +0200

    Finally got that shit to work, testing various paramters now

--- neuralNet.py
+++ neuralNet.py
@@ -1,10 +1,12 @@
-import numpy as np
 import matplotlib.pylab as plt
+import numpy as np
 import sys
+import os
+from LogisticRegression import get_data
 
 class NeuralNet():
 
-    def __init__(self, xData, yData, nodes=[1, 10, 1], activations=['sigmoid', None], cost_function='mse', regularization=None, lamda=0.0):
+    def __init__(self, xData, yData, nodes=[23, 10, 1], activations=['sigmoid', None], cost_function='mse', regularization=None, lamda=0.0):
         """
         This method is used to create a Multi Layered Perceptron (MLP)
         
@@ -43,7 +45,7 @@ class NeuralNet():
         self.activations = activations
         self.nLayers = len(activations)
 
-        self.split_data(folds=10, frac=0.3, shuffle=True)
+        self.split_data(folds=10, test_size=0.2, shuffle=True)
         self.initialize_weights_biases()
 
     def split_data(self, folds=None, test_size=None, shuffle=False):
@@ -68,10 +70,6 @@ class NeuralNet():
         if shuffle:
             xData = self.xData.iloc[ np.random.permutation(self.N) ]
             yData = self.yData.iloc[ np.random.permutation(self.N) ]
-            # randomize = np.arange(self.N)
-            # np.random.shuffle(randomize)
-            # xData = xData[randomize]
-            # yData = yData[randomize]
 
         if folds != None:
             xFolds = np.array_split(xData, folds, axis = 0)
@@ -103,24 +101,34 @@ class NeuralNet():
         None
         """
 
-        self.Weights = {}
-        self.Biases = {}
-        self.Weights_grad = {}
-        self.Biases_grad = {}
-        self.Z = {}
-        self.A = {}
-        self.C = {}
+        self.Weights        = {}
+        self.Biases         = {}
+        self.Weights_grad   = {}
+        self.Biases_grad    = {}
+        self.A              = {}
 
         for i in range(len(self.activations)):
 
-            self.Weights['W'+str(i+1)] = np.random.uniform(-0.1, 0.1, (self.nodes[i], self.nodes[i+1]))
-            self.Biases['B'+str(i+1)] = np.random.uniform(-0.1, 0.1, self.nodes[i+1])
+            if self.activations[i] == 'sigmoid':
+                r = np.sqrt(6.0 / (self.nodes[i] + self.nodes[i+1]))
+                self.Weights['W'+str(i+1)] = np.random.uniform(-r, r, size=(self.nodes[i], self.nodes[i+1]))
+                self.Biases['B'+str(i+1)]  = np.random.uniform(-r, r, self.nodes[i+1])
+            elif self.activations[i] == 'tanh':
+                r = 4.0 * np.sqrt(6.0 / (self.nodes[i] + self.nodes[i+1]))
+                self.Weights['W'+str(i+1)] = np.random.uniform(-r, r, size=(self.nodes[i], self.nodes[i+1]))
+                self.Biases['B'+str(i+1)]  = np.random.uniform(-r, r, self.nodes[i+1])
+            elif self.activations[i] == 'relu':
+                self.Weights['W'+str(i+1)] = np.random.normal(size=(self.nodes[i], self.nodes[i+1])) * np.sqrt(2.0 / self.nodes[i])
+                self.Biases['B'+str(i+1)]  = np.random.normal(self.nodes[i+1]) * np.sqrt(2.0 / self.nodes[i])
+            else :
+                self.Weights['W'+str(i+1)] = np.random.normal(size=(self.nodes[i], self.nodes[i+1]))
+                self.Biases['B'+str(i+1)]  = np.random.normal(self.nodes[i+1])
 
             self.Weights_grad['dW'+str(i+1)] = np.zeros_like(self.Weights['W'+str(i+1)])
             self.Biases_grad['dB'+str(i+1)] = np.zeros_like(self.Biases['B'+str(i+1)])
 
 
-    def activation(self, x, act_func):
+    def activation_function(self, x, act_func, derivative=False):
         """
         Calculate the activation function
         INPUT:
@@ -129,51 +137,26 @@ class NeuralNet():
             data that should be chased through the activation function
         act_func: string
             string stating the activation function wanted ['sigmoid', 'relu', 'tanh']
+        derivative: boolean
+            flag indicating whether the derivative should be retrieved or not
 
         OUTPUT:
         ----------
             returns the desired activation function results.
         """
         if act_func == 'sigmoid':
-            return 1.0/(1.0 + np.exp(-x))
+            with np.errstate(divide='ignore'): # some runtimeWarnings
+                return x * (1 - x) if derivative else 1/(1+np.exp(-x))
 
         elif act_func == 'tanh':
-            return np.tanh(x)
+            return 1 - x**2 if derivative else np.tanh(x)
 
         elif act_func == 'relu':
-            return x * (x >= 0)
+            return 1*(x >= 0) if derivative else x * (x >= 0)
 
         elif act_func == None: # identity
-            return x
+            return 1 if derivative else x
 
-        else:
-            raise ValueError("You have to select a proper activation function of ['sigmoid', 'relu', 'tanh', None].\n Given: {}".format(act_func))
-
-    def activation_derivative(self, x, act_func):
-        """
-        Calculate the activation function derivatives
-        INPUT:
-        ---------
-        x: numpy ndarray
-            data that should be chased through the activation function
-        act_func: string
-            string stating the activation function wanted ['sigmoid', 'relu', 'tanh']
-
-        OUTPUT:
-        ----------
-            returns the desired activation function results.
-        """
-        if act_func == 'sigmoid':
-            return x*(1 - x)
-
-        elif act_func == 'tanh':
-            return 1 - x**2
-
-        elif act_func == 'relu':
-            return 1*(x >= 0)
-
-        elif act_func == None:
-            return 1
         else:
             raise ValueError("You have to select a proper activation function of ['sigmoid', 'relu', 'tanh', None].\n Given: {}".format(act_func))
 
@@ -205,10 +188,10 @@ class NeuralNet():
         """
 
         if self.cost_func == 'mse':
-            cost =  0.5 / y.shape[0] * np.sum( (y - ypred) ** 2)
+            cost =  0.5 / y.reshape(-1, 1).shape[0] * np.sum( (y.reshape(-1, 1) - ypred) ** 2)
 
         if self.cost_func == 'log':
-            cost = -0.5 / y.shape[0] * np.sum( np.log(ypred[np.arange(ypred.shape[0]), y.flatten()]) )
+            cost = -0.5 / y.reshape(-1, 1).shape[0] * np.sum( np.log(ypred[np.arange(ypred.shape[0]), y.reshape(-1, 1).flatten()]) )
 
         if self.regularization == 'l2':
             for key in list(self.Weights.keys()):
@@ -237,7 +220,7 @@ class NeuralNet():
         """
 
         if self.cost_func == 'mse':
-            return -1.0 / y.shape[0] * (y - ypred)
+            return -1.0 / y.shape[0] * (y - ypred.flatten())
 
         elif self.cost_func == 'log':
             ypred[ np.arange(ypred.shape[0]), y.flatten() ] -= 1
@@ -279,10 +262,8 @@ class NeuralNet():
 
         self.A['A0'] = x
         for i in range(self.nLayers):
-
             z = np.dot( self.A['A'+str(i)], self.Weights['W'+str(i+1)] ) + self.Biases['B'+str(i+1)]
-            a = self.activation(z, self.activations[i])
-            self.Z['Z'+str(i+1)] = z
+            a = self.activation_function(z, self.activations[i])
             self.A['A'+str(i+1)] = a
 
         if self.cost_func == 'log':
@@ -298,6 +279,7 @@ class NeuralNet():
         """
         doing the funny backpropagation
         INPUT:
+        --------
         yTrue: numpy ndarray
             Correct values of the targets y
         """
@@ -308,8 +290,11 @@ class NeuralNet():
             if i == self.nLayers:
                 c = self.cost_function_derivative(yTrue, self.output)
             else:
-                c = np.dot(c, self.Weights['W'+str(i+1)].T)
-                c = c * self.activation_derivative(self.A['A'+str(i)], self.activations[i-1])
+                try:
+                    c = np.dot(c, self.Weights['W'+str(i+1)].T)
+                except: # for the very first iteration
+                    c = np.dot(c, self.Weights['W'+str(i+1)])
+                c = c * self.activation_function(self.A['A'+str(i)], self.activations[i-1], derivative=True)
 
             grad_w = np.dot(self.A['A'+str(i-1)].T, c)
             grad_b = np.sum(c, axis= 0)
@@ -324,10 +309,13 @@ class NeuralNet():
                 self.Weights['W'+str(i)] -= self.eta * (grad_w + self.lamb*np.sign(self.Weights['W'+str(i)]))
 
             else:
-                self.Weights['W'+str(i)] -= self.eta * grad_w
+                #updateSize = (self.eta * grad_w).reshape(-1, 1)
+                try:
+                    self.Weights['W'+str(i)] -= (self.eta * grad_w).reshape(-1, 1)
+                except:
+                    self.Weights['W'+str(i)] -= (self.eta * grad_w)
 
             self.Biases['B'+str(i)] -= self.eta * grad_b
-
 
 
     def trainingNetwork(self, epochs=1000, batchSize=200, tau=0.01, n_print=100):
@@ -360,9 +348,12 @@ class NeuralNet():
             for b in range(num_batch):
                 self.eta = eta(epoch*num_batch+b)
                 # get the batches 
-                batch = indices[b*batchSize:(b+1)*batchSize]
-                xBatch = self.xTrain[batch]
+                batch = indices[b*batchSize : (b+1)*batchSize]
                 yBatch = self.yTrain[batch]
+
+                #self.xTrain.set_index(np.arange(self.xTrain.shape[0]), inplace=True)
+                xBatch = self.xTrain.iloc[batch]
+
                 # propagate them through the network twice (forward and backward)
                 self.feed_forward(xBatch)
                 self.backpropagation(yBatch)
@@ -372,11 +363,26 @@ class NeuralNet():
                 ypred_test  = self.feed_forward(self.xTest, isTraining=False)
                 trainError  = self.cost_function(self.yTrain, ypred_train)
                 testError   = self.cost_function(self.yTest, ypred_test)
-                print("{} epoch: Training Error:  {}, Test Error  {}".format(epoch, trainError, testError))
+                print("[ERROR]: {} epoch from {}: Training Error:  {}, Test Error  {}".format(epoch, epochs+1, trainError, testError))
 
                 if self.cost_func == 'log':
                     trainAcc = self.accuracy(self.yTrain, ypred_train)
                     testAcc = self.accuracy(self.yTest, ypred_test)
-                    print("Accuracy after %i epochs, Training:   %g %%, Test:   %g %%\n" %(epoch, trainAcc, testAcc))
+                    print("[ACCURACY]: {} epoch from {}: Training Acc:  {}, Test Acc  {}".format(epoch, epochs+1, trainAcc, testAcc))
                     self.convergence_rate['Epoch'].append(epoch)
                     self.convergence_rate['Test Accuracy'].append(testAcc)
+
+## TESTING MY BABY 
+if __name__ == '__main__':
+    # read the dataset
+    cwd = os.getcwd()
+    filename = "\\default of credit card clients.xls"
+    filePath = cwd + filename
+    X, y = get_data(filePath, standardized=False, normalized=False)
+    neuralNet = NeuralNet(X, y, nodes=[23, 64, 64, 128, 1], activations=['relu', 'relu','relu', 'sigmoid'])
+    neuralNet.split_data(test_size=0.2)
+    neuralNet.trainingNetwork(epochs=250, batchSize=128, tau=0.01)
+
+    ypred_test = neuralNet.feed_forward(neuralNet.xTest, isTraining=False)
+    acc = neuralNet.accuracy(neuralNet.yTest, ypred_test)
+    print("Accuracy: {}".format(acc))
